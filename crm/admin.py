ad7f3ee0615040b505710b364fbf6919679c4cb9
Patrick Rauscher <prauscher@ohai.su>
2013-09-05 20:25:35 +0000

    add tagsupport to crm

--- crm/admin.py
+++ crm/admin.py
@@ -1,4 +1,5 @@
-from crm.models import Entity
+from crm.models import Entity, Tag
 from django.contrib import admin
 
 admin.site.register(Entity)
+admin.site.register(Tag)
--- crm/models.py
+++ crm/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class Entity(models.Model):
+	tags = models.ManyToManyField("Tag")
 	memo = models.TextField()
+
+class Tag(models.Model):
+	label = models.CharField(max_length=30)
