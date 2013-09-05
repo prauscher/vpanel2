9cff1eb1b798ee02c6af47526d117ae132ea964d
Patrick Rauscher <prauscher@ohai.su>
2013-09-05 21:12:41 +0000

    allow entities without tag,link or memo

--- crm/models.py
+++ crm/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Entity(models.Model):
-	links = models.ManyToManyField("self")
-	tags = models.ManyToManyField("Tag")
-	memo = models.TextField()
+	links = models.ManyToManyField("self", blank = True)
+	tags = models.ManyToManyField("Tag", blank = True)
+	memo = models.TextField(blank = True)
 
 class Tag(models.Model):
 	label = models.CharField(max_length=30)
