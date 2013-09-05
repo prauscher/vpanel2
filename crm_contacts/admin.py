05335cf6833899e7180f6453b97b6f71d3fda038
Patrick Rauscher <prauscher@ohai.su>
2013-09-05 19:01:50 +0000

    add fields to crm_contacts

--- crm_contacts/admin.py
+++ crm_contacts/admin.py
@@ -1,4 +1,8 @@
-from crm_contacts.models import Contact
+from crm_contacts.models import Contact, NaturalPerson, ArtificialPerson, TelephoneNumber, Email
 from django.contrib import admin
 
 admin.site.register(Contact)
+admin.site.register(NaturalPerson)
+admin.site.register(ArtificialPerson)
+admin.site.register(TelephoneNumber)
+admin.site.register(Email)
--- crm_contacts/models.py
+++ crm_contacts/models.py
@@ -3,3 +3,23 @@ from crm.models import Entity
 
 class Contact(models.Model):
 	entity = models.ForeignKey(Entity)
+
+class NaturalPerson(models.Model):
+	contact = models.ForeignKey(Contact)
+	givenName = models.CharField(max_length=30)
+	surName = models.CharField(max_length=50)
+	birthDay = models.DateField()
+
+class ArtificialPerson(models.Model):
+	contact = models.ForeignKey(Contact)
+	legalName = models.CharField(max_length=100)
+	contactGivenName = models.CharField(max_length=50)
+	contactSurName = models.CharField(max_length=30)
+
+class TelephoneNumber(models.Model):
+	contact = models.ForeignKey(Contact)
+	telephone = models.CharField(max_length=20)
+
+class Email(models.Model):
+	contact = models.ForeignKey(Contact)
+	email = models.EmailField()
