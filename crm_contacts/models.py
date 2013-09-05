d0822512d0923c7b83b864439072193c6268b0c3
Patrick Rauscher <prauscher@ohai.su>
2013-09-05 20:04:35 +0000

    change contactentities

--- crm_contacts/models.py
+++ crm_contacts/models.py
@@ -3,23 +3,23 @@ from crm.models import Entity
 
 class Contact(models.Model):
 	entity = models.ForeignKey(Entity)
+	naturalPerson = models.ForeignKey("NaturalPerson", null = True, blank = True)
+	artificialPerson = models.ForeignKey("ArtificialPerson", null = True, blank = True)
+	telephoneNumbers = models.ManyToManyField("TelephoneNumber")
+	emails = models.ManyToManyField("Email")
 
 class NaturalPerson(models.Model):
-	contact = models.ForeignKey(Contact)
 	givenName = models.CharField(max_length=30)
 	surName = models.CharField(max_length=50)
 	birthDay = models.DateField()
 
 class ArtificialPerson(models.Model):
-	contact = models.ForeignKey(Contact)
 	legalName = models.CharField(max_length=100)
 	contactGivenName = models.CharField(max_length=50)
 	contactSurName = models.CharField(max_length=30)
 
 class TelephoneNumber(models.Model):
-	contact = models.ForeignKey(Contact)
 	telephone = models.CharField(max_length=20)
 
 class Email(models.Model):
-	contact = models.ForeignKey(Contact)
 	email = models.EmailField()
