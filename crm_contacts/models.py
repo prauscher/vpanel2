c3991a881d0e56650e6b80baa9d388e872156f95
Patrick Rauscher <prauscher@ohai.su>
2013-09-05 21:12:49 +0000

    add some stingrepresentations

--- crm_contacts/models.py
+++ crm_contacts/models.py
@@ -5,21 +5,36 @@ class Contact(models.Model):
 	entity = models.ForeignKey(Entity)
 	naturalPerson = models.ForeignKey("NaturalPerson", null = True, blank = True)
 	artificialPerson = models.ForeignKey("ArtificialPerson", null = True, blank = True)
-	telephoneNumbers = models.ManyToManyField("TelephoneNumber")
+	telephoneNumbers = models.ManyToManyField("TelephoneNumber", blank = True)
 	emails = models.ManyToManyField("Email")
+
+	def __unicode__(self):
+		return str(self.artificialPerson) if self.naturalPerson == None else str(self.naturalPerson)
 
 class NaturalPerson(models.Model):
 	givenName = models.CharField(max_length=30)
 	surName = models.CharField(max_length=50)
 	birthDay = models.DateField()
 
+	def __unicode__(self):
+		return self.givenName + " " + self.surName
+
 class ArtificialPerson(models.Model):
-	legalName = models.CharField(max_length=100,unique=True)
+	legalName = models.CharField(max_length=100, unique=True)
 	contactGivenName = models.CharField(max_length=50)
 	contactSurName = models.CharField(max_length=30)
+
+	def __unicode__(self):
+		return self.legalName
 
 class TelephoneNumber(models.Model):
 	telephone = models.CharField(max_length=20, unique=True)
 
+	def __unicode__(self):
+		return self.telephone
+
 class Email(models.Model):
 	email = models.EmailField(unique=True)
+
+	def __unicode__(self):
+		return self.email
