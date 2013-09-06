441dca07521e8219eaf7a91d78b0218af3ef1fd5
Patrick Rauscher <prauscher@ohai.su>
2013-09-06 16:11:43 +0000

    add informations about classes

--- crm/models.py
+++ crm/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Entity(models.Model):
+	"""
+	An Entity is the base of every part of the CRM.
+	"""
+
 	links = models.ManyToManyField("self", blank = True)
 	tags = models.ManyToManyField("Tag", blank = True)
 	memo = models.TextField(blank = True)
@@ -20,16 +24,28 @@ class Entity(models.Model):
 		return str(self.getEntity())
 
 class Tag(models.Model):
+	"""
+	Each Entity may be linked to as many Tags as you like. They may be used for filtering as well
+	"""
+
 	label = models.CharField(max_length=30)
 
 	def __unicode__(self):
 		return self.label
 
 class Process(models.Model):
+	"""
+	Each change of a Entity is done via a Process which will be archived for later research
+	"""
+
 	start = models.DateTimeField()
 	user = models.ForeignKey(User)
 
 class Filter(models.Model):
+	"""
+	A Filter may be used to identify only the Entities you are looking for. This is the baseclass
+	"""
+
 	name = models.CharField(max_length=70)
 
 class Invariant(models.Model):
